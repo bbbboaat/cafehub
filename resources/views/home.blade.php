ccabbc910ae718ba3989e96a0dde8bfcbb345321
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-20 01:25:35 +0700

    importHomepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafehub</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+
+<header>
+    <a href="#" class="logo"><i class="fas fa-utensils"></i>food</a>
+</header>
+
+<script src="js/script.js"></script>
+</body>
+</html>
