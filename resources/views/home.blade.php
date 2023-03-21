a9e9a8fd31f489f41e2fc586c640e87792d23378
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-21 17:04:47 +0700

    profilebtn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,16 +24,17 @@
         <a href="#popular">POPULAR</a>
         <a href="#menu">MENU</a>
 
-        @if (Route::has('login'))
-                    @auth
-                    <x-app-layout>
-                    </x-app-layout>
 
+        @if (Route::has('login'))
+
+                    @auth
+
+                        <a href="{{ url('/dashboard') }}" class="btn">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <a href="{{ route('login') }}" class="btn">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            <a href="{{ route('register') }}" class="btn">Register</a>
                         @endif
                     @endauth
                 </div>
