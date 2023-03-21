3697ef18d29ce5109b8a05e49087783d100274ba
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-21 15:34:04 +0700

    profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,9 @@
 
         @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    <x-app-layout>
+                    </x-app-layout>
+
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
