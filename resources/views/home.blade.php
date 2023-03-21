79f1e4fc28b8cf0b4462809ae2157b94d82b6fdf
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-21 15:19:14 +0700

    homeLogin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,12 +18,29 @@
 <header>
     <a href="#" class="logo"><i class="fa-sharp fa-solid fa-mug-saucer"></i>CAFEHUB</a>
     <div id="menu-bar" class="fas fa-bars"></div>
-    <nav class="navbar">
+    <ul class="navbar">
         <a href="#home">HOME</a>
         <a href="#speciality">SPECIALITY</a>
         <a href="#popular">POPULAR</a>
         <a href="#menu">MENU</a>
-    </nav>
+
+        @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+
+    </ul>
+
+
 </header>
 
 
